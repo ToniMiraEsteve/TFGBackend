d8b8ed264b14c92355e82870e606d5618b96308d
Toni Mira Esteve <tonimiraesteve@gmail.com>
2025-05-04 20:46:58 +0200

    Add role field to UserFactory and remove UsuarioFactory

--- BackendTFG/database/factories/UserFactory.php
+++ BackendTFG/database/factories/UserFactory.php
@@ -28,6 +28,7 @@ class UserFactory extends Factory
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => bcrypt('password'),
+            'rol' => $this->faker->randomElement(['admin', 'junta', 'monitor','usuario']),
             'remember_token' => Str::random(10),
         ];
     }
